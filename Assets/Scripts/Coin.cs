18e34ac4a9cb81094549c4b7c552400334c88948
GameDev-HSMZ <sebastianlawall@web.de>
2016-02-09 19:01:35 +0100

    Coin Sound onTriggerEnter

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -3,9 +3,17 @@ using System.Collections;
 
 public class Coin : MonoBehaviour {
 
+	private AudioSource source;
+	public AudioClip coinSound;
+
+	void Awake () {
+		source = GetComponent<AudioSource> ();
+	}
+
 	void OnTriggerEnter2D(Collider2D other) {
 		if (other.gameObject.CompareTag ("Player")) {
 			CoinCount.coinCount ++;
+			AudioSource.PlayClipAtPoint(coinSound, transform.position); //source.clip = coinSound; source.Play(); not working?!
 			Destroy (gameObject);
 		}
 	}
