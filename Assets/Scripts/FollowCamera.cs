80547572c8f0ae815044cb8207e59c1510e5037b
boebech <christoph.boebel@googlemail.com>
2016-05-23 20:20:57 +0200

    FollowCamera

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -8,6 +8,9 @@ public class FollowCamera : MonoBehaviour {
 	[SerializeField]
 	private Vector3 offset;
 	private float cameraFollowSpeed = 5f;
+
+	private float restartDelay = 5f;
+	private float restartTimer;
 	
 	// Update is called once per frame
 	void Update () {
@@ -15,6 +18,18 @@ public class FollowCamera : MonoBehaviour {
 
 		//transform.position = Vector3.Lerp (transform.position, newPosition, cameraFollowSpeed * Time.deltaTime);
 		transform.position = newPosition;
+
+
+		// increment a timer to count up to switching angle
+		restartTimer = restartTimer + Time.deltaTime;
+
+		if(restartTimer >= restartDelay){
+
+			//reset counter so that EndGame() can be called again
+			offset.z = offset.z - 7;
+			restartTimer = 0f;
+
+		}
 	
 	}
 }
