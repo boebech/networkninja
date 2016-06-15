a5538bf84b11023ebd4fc7c429691b21c673c16e
GameDev-HSMZ <sebastianlawall@web.de>
2016-06-15 20:01:31 +0200

    multiple background music bug fixed

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioManager : MonoBehaviour {
+
+	public static bool isBackgroundMusicPlaying = false;
+
+
+}
--- Assets/Scripts/ChangeMusic.cs
+++ Assets/Scripts/ChangeMusic.cs
@@ -11,17 +11,27 @@ public class ChangeMusic : MonoBehaviour {
 	// Use this for initialization
 	void Awake () {
 		source = GetComponent<AudioSource> ();
+		if (AudioManager.isBackgroundMusicPlaying) {
+			//Debug.Log ("source is playing");
+		} else {
+			//Debug.Log ("source was not playing. start now...");
+			source.Play();
+			AudioManager.isBackgroundMusicPlaying = true;
+		}
+			
 	}
 
 	void OnLevelWasLoaded (int level){
 		if(level == 1 /*&& wasRunningBefore==false*/){
 			source.clip = level1Music;
 			source.Play ();
+			AudioManager.isBackgroundMusicPlaying = false;
 			//wasRunningBefore=true;
 		}
 		if(level == 2 /*&& wasRunningBefore==false*/){
 			source.clip = level2Music;
 			source.Play ();
+			AudioManager.isBackgroundMusicPlaying = false;
 			//wasRunningBefore=true;
 		}
 	}
