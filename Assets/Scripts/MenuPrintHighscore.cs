3be6b03e8f9bc9b28c6334d411136c91aab9deb5
GameDev-HSMZ <sebastianlawall@web.de>
2016-06-29 21:31:01 +0200

    MenuScene Highscore display

---
+++ Assets/Scripts/MenuPrintHighscore.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MenuPrintHighscore : MonoBehaviour {
+
+	public Text highscore;
+
+	// Use this for initialization
+	void Start () {
+		highscore.text = "NETWORKNINJA (SCORE: " + PlayerPrefs.GetInt ("CoinCount", 0) + ")";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
